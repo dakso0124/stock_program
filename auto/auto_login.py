5a76fdec77e2415affb81cb59a5d70b050809009
Dakso <dakso0124@gmail.com>
2021-04-11 16:47:23 +0900

    - 자동 로그인 추가

---
+++ auto/auto_login.py
@@ -0,0 +1,46 @@
+import ctypes
+import os
+import time
+
+import win32com.client
+from pywinauto import application
+
+
+class AutoLogin:
+    def __init__(self):
+        pass
+
+    def login(self):  # 로그인
+        os.system('taskkill /IM coStarter* /F /T')
+        os.system('taskkill /IM CpStart* /F /T')
+        os.system('taskkill /IM DibServer* /F /T')
+
+        os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+        os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+        os.system('wmic process where "name like \'%DibServer%\'" call terminate')
+
+        time.sleep(5)
+        app = application.Application()
+        app.start('C:\daishin\STARTER\\ncStarter.exe /prj:cp /id:아이디 /pwd:비밀번호 /pwdcert:인증서비밀번호 /autostart')
+        time.sleep(30)
+
+        return self.check_connect()    # 연결 실패시 10초 후 재시도
+
+
+    def check_connect(self):  # 연결 확인
+        cpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
+        cpTradeUtil = win32com.client.Dispatch('CpTrade.CpTdUtil')
+
+        if not ctypes.windll.shell32.IsUserAnAdmin():  # 관리자 권한 체크
+            print('check system : admin user fail')
+            return False
+
+        if cpStatus.IsConnect == 0:  # 사이보스 연결 체크
+            print('check system : connect server fail')
+            return False
+
+        if cpTradeUtil.TradeInit(0) != 0:  # 거래 초기화 체크
+            print('check system : init trade fail')
+            return False
+        print('success connect')
+        return True
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import stock_kind as sk
 import collect_day.collect_day as cd
 import real_time.real_time_price as rt
 import naver_crawling.naver_finance_crawling as nfc
+import auto.auto_login as at
+import multiple_real_time_finding_listmethod as real_time
+
 
 
 def start_collect_stock_kind():
@@ -33,11 +36,27 @@ def start_collect_real_time(code=None):     # 실시간 - 정상작동하지 않
     collect_real_time = rt.RealTimeCollect()
     collect_real_time.start_collect_real_time_data(code)
 
+
+def start_get_real_time():
+    real_time.start()
+
+
+def start_program():
+    auto_login = at.AutoLogin()
+    while not auto_login.login():
+        pass
+
+    codes = start_collect_stock_kind()
+    # start_collect_day_update(codes) # 모든 종목 일별 데이터 갱신
+    start_get_real_time()
+
 # IT서비스, 해운사, 조선, 광고, 건설
 
-codes = start_collect_stock_kind()
-# start_collect_day(codes)
-start_collect_day_update(codes)
+start_program()
+
+
+# start_collect_day(codes)  # 모든 종목 일별 데이터 수집(전체)
+# start_collect_day_update(codes) # 모든 종목 일별 데이터 갱신
 # start_real_time_it_service(('IT서비스', '해운사', '조선', '광고', '건설'))
 
 
